c187a29a80b63aa61a39641d98256a3c021bac17
raja <rajadodve43@gmail.com>
2026-03-16 18:39:22 +0530

    Update levels images

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,9 @@ mod levels;
 fn main() {
     if levels::level0::run() {
         if levels::level1::run() {
-            levels::level2::run();
+            if levels::level2::run() {
+                levels::level3::run();
+            }
         }
     }
 }
